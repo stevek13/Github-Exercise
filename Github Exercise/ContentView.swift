01b5106f429cb34ba996c3a59b0dd6080ac2bbfc
Steve Kite <skite1@hotmail.com>
2022-10-24 07:42:04 -0700

    Version 1.0 - Red

--- Github Exercise/ContentView.swift
+++ Github Exercise/ContentView.swift
@@ -3,20 +3,23 @@
 //  Github Exercise
 //
 //  Created by Steve on 10/24/22.
-//
+//  Showing why to not use space in names when using github
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+                
+            Text("Version.1.0")
+                .font(.largeTitle)
+                .fontWeight(.bold)
         }
         .padding()
-    }
+        .foregroundColor(.red)    }
 }
 
 struct ContentView_Previews: PreviewProvider {
